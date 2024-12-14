e00ac162b042fc59d4a6ed27c71c644bb6e1cf2e
Flavia Martinez <flavia.martinez@uc.cl>
2024-12-13 21:19:50 -0300

    create arrays for both pages

---
+++ assets/js/alquiler.js
@@ -0,0 +1,46 @@
+const propiedades_alquiler = [
+	{
+		nombre: 'Apartamento en el centro de la ciudad',
+		src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+		descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+		ubicacion: '123 Main Street, Anytown, CA 91234',
+		habitaciones: 2,
+		costo: 2.000,
+		smoke: false,
+		pets: true
+
+	},
+	{
+		nombre: 'Apartamento luminoso con vista al mar',
+		src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+		descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+		ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+		habitaciones: 3,
+		costo: 2.500,
+		smoke: true,
+		pets: true
+
+	},
+	{
+		nombre: 'Condominio moderno en zona residencial',
+		src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+		descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+		ubicacion: '123 Main Street, Anytown, CA 91234',
+		habitaciones: 2,
+		costo: 0,
+		smoke: false,
+		pets: false
+
+	},
+	{
+		nombre: 'Casa luminosa con jardín en zona tranquila',
+		src: '',
+		descripcion: 'Una casa espaciosa, llena de luz natural, con un amplio jardín ideal para reuniones y tiempo al aire libre.',
+		ubicacion: 'Avenida Del Parque 32, Colonia Las Flores, MX 22045',
+		habitaciones: 4,
+		costo: 1300,
+		smoke: false,
+		pets: true
+
+	}
+]
